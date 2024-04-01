31ade0c27efbc1f8eb3c1d3e1c8da7adfcecbf1f
HarleyMartins <harleyfla05@gmail.com>
2024-04-01 20:42:41 -0300

    created REDDIT MODAL

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,19 @@
 function toggleLike(button) {
   button.classList.toggle("clicked")
 }
+
+// FUNÇÃO ABRIR/FECHAR MODAL DE NOVO REDDIT
+
+const buttonModal = document
+  .getElementById("criarReddit")
+  .addEventListener("click", function () {
+    const modal = document.getElementById("modal")
+    modal.classList.add("open")
+  })
+
+const buttonCloseModal = document
+  .getElementById("cancelReddit")
+  .addEventListener("click", function () {
+    const modal = document.getElementById("modal")
+    modal.classList.remove("open")
+  })
